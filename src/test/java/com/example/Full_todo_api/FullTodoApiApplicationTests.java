2cd30294658a5c9b7c7f0ae23069145230cf8e5d
sitimi <sitimi522@gmail.com>
2024-06-25 11:20:07 +0900

    first_commit

---
+++ src/test/java/com/example/Full_todo_api/FullTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Full_todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
