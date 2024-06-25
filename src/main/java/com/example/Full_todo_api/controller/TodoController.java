b46c15815810470a31344f3acf48444a2b96ed62
sitimi <sitimi522@gmail.com>
2024-06-25 11:50:40 +0900

    ルートへのアクセス確認

--- src/main/java/com/example/Full_todo_api/controller/TodoController.java
+++ src/main/java/com/example/Full_todo_api/controller/TodoController.java
@@ -1,5 +1,17 @@
 package com.example.Full_todo_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/todos")
 public class TodoController {
 
+	@GetMapping
+	public String hello() {
+		return "Hello world";
+	}
+	
 }
