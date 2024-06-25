ef036f5344f27e0ffa716ef2e80c4a21ec169290
sitimi <sitimi522@gmail.com>
2024-06-25 17:59:11 +0900

    CORS許可設定

--- src/main/java/com/example/Full_todo_api/controller/TodoController.java
+++ src/main/java/com/example/Full_todo_api/controller/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.example.Full_todo_api.mapper.TodoMapper;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/todos")
 public class TodoController {
 	
